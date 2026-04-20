957d1e4581e2708838b8c0aa343266fcf76764a5
Atilus <ildar2003ham@gmail.com>
2026-04-20 13:44:57 +0500

    msg

---
+++ src/features/stats/TodoInfo.jsx
@@ -0,0 +1,36 @@
+import {memo, useContext, useMemo} from 'react'
+import { TasksContext } from '@/entities/todo'
+
+const TodoInfo = (props) => {
+  const { styles } = props
+
+  const {
+    tasks,
+    deleteAllTasks,
+  } = useContext(TasksContext)
+
+  const total = tasks.length
+  const hasTasks = total > 0
+  const done = useMemo(() => {
+    return tasks.filter(({ isDone }) => isDone).length
+  }, [tasks])
+
+  return (
+    <div className={styles.info}>
+      <div className={styles.totalTasks}>
+        Сделано {done} из {total}
+      </div>
+      {hasTasks && (
+        <button
+          className={styles.deleteAllButton}
+          type="button"
+          onClick={deleteAllTasks}
+        >
+          Удалить всё
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default memo(TodoInfo)
